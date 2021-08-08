6c088b0625569ace4bcbfaace27febb00745ec01
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-08 01:47:54 +0100

    new routes for company informations

---
+++ src/pages/company/history.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function history() {
+  return <div>this is our history</div>
+}
+
+export default history
---
+++ src/pages/company/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function index() {
+  return <div>hello from company</div>
+}
+
+export default index
