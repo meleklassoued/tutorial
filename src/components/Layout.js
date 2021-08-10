a23a92457a73fe226f9aab48dff11bb4bc30b8ef
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-10 01:34:18 +0100

    movecss to foalder css inAssets and import it to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,8 @@ import React from "react"
 import Navbar from "./navbar"
 import Footer from "./Footer"
 
+import "../assets/css/main.css"
+import "normalize.css"
 const Layout = props => {
   console.log(props)
   return (
