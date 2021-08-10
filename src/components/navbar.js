3fa2b7eb3aeb20faed50f5fc8e9e169a10994ead
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-10 17:03:32 +0100

    Design the navBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { FiAlignJustify } from "react-icons/fi"
+import logo from "../assets/images/logo.svg"
 const Navbar = props => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-        <li>
-          <Link to="/contact">contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">recipes</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <button className="nav-btn">
+            <FiAlignJustify></FiAlignJustify>
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            Home
+          </Link>
+          <Link to="/recipes" className="nav-link" activeClassName="active-link">
+            recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            about
+          </Link>
+          <div className="nav-link contact-link">
+          <Link to="/contact" className="btn">Contact</Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
