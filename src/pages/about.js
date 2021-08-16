704b1e0d26883822b9a91a970bfb1ad64225118f
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-16 03:33:01 +0100

    change things in the about Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,43 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { page, text } from "../css exampels/about.module.css"
-const about = () => {
+// import { page, text } from "../css exampels/about.module.css"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
+const About = () => {
   return (
     <Layout>
-      <div className={page}>
-        <h1>about Page</h1>
-        <p className={text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eos
-          earum veniam soluta explicabo illum. Deleniti qui dolores possimus
-          temporibus accusantium, facere tempore ex reprehenderit sapiente? Sint
-          officiis nostrum quos aliquid vel. Voluptatibus ea libero iusto?
-          Quibusdam repellat, non saepe quisquam officiis repellendus, odit,
-          amet rerum natus dolores mollitia placeat!
-        </p>
-      </div>
+      <main className="page">
+        <section className="about-page">
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <article>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+              asperiores expedita temporibus saepe ipsum, iure debitis,
+              voluptates necessitatibus repellendus corrupti et recusandae quasi
+              odio, nam harum cumque amet ratione. Obcaecati aspernatur,
+              mollitia .
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Mollitia, laboriosam aspernatur.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              labore quaerat minus magnam animi eius optio veritatis facere
+              sapiente sed!
+            </p>
+          <Link to="/contact" className="btn">
+            contact
+          </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="person pouring salt"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default about
+export default About
