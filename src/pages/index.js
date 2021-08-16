0b4e92a5ae1a9e64bf63bd6b63b74e62f6dbb603
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-16 02:56:42 +0100

    clean Up my index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
-// import { ExampleButton } from "../css exampels/button"
-import Images from "../css exampels/image"
+
 export default function Home() {
   return (
     <>
       <Layout>
         <h1>hello people</h1>
-       <Images/>
       </Layout>
     </>
   )
-} 
+}
